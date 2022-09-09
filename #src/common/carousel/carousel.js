e6fdc9318b9c6d07dadd916cf3e8767820854c8e
Spacespire <aleksandra@spacespire.com>
2022-09-09 13:20:46 +0300

    fix

--- #src/common/carousel/carousel.js
+++ #src/common/carousel/carousel.js
@@ -1,27 +1,29 @@
 {
-    let carousels = document.querySelectorAll('[data-carousel]');
+    let carousels = document.querySelectorAll('[data-slider="carousel"]');
     if(carousels.length) {
         carousels.forEach(carousel => {
             let carouselSwiper = new Swiper(carousel.querySelector('.swiper'), {
-                speed: 800,
+                observer: true,
+                observeParents: true,
+                speed: 500,
+                pagination: {
+                    el: carousel.querySelector('.swiper-pagination'),
+                    clickable: true,
+                },
                 navigation: {
-                    nextEl: carousel.querySelector('[data-action="btn-next"]'),
-                    prevEl: carousel.querySelector('[data-action="btn-prev"]'),
+                    nextEl: carousel.querySelector('.slider-button--next'),
+                    prevEl: carousel.querySelector('.slider-button--prev'),
                 },
                 breakpoints: {
-                    320: {
-                        slidesPerView: 2,
-                        spaceBetween: 15,
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 25,
                         autoHeight: true,
                     },
                     768: {
                         slidesPerView: 3,
-                        spaceBetween: 20,
-                    },
-                    992: {
-                        slidesPerView: 4,
-                        spaceBetween: 25,
-                    },
+                        spaceBetween: 30,
+                    }
                 },
             });
         })
