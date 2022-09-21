41fee028aaafec53b91de3f00f590ba528867a90
Spacespire <aleksandra@spacespire.com>
2022-09-21 13:29:50 +0300

    fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -376,16 +376,18 @@ class App {
     if (header) {
         let mobileMenu = document.querySelector('[data-mobile-menu]');
         let btnMobileMenuClose = document.querySelector('[data-action="close-mobile-menu"]');
-        let burger = document.querySelector('[data-action="open-mobile-menu"]');
+        let burger = burgerHandler();
 
 
-        burger.addEventListener('click', () => {
-            mobileMenu.classList.add('mobile-menu--open');
-            document.documentElement.classList.add('overflow-hidden');
+        burger.el.addEventListener('click', () => {
+            mobileMenu.classList.toggle('mobile-menu--open');
+            document.documentElement.classList.toggle('overflow-hidden');
+            burger.toggle();
         })
         btnMobileMenuClose.addEventListener('click', () => {
             mobileMenu.classList.remove('mobile-menu--open');
             document.documentElement.classList.remove('overflow-hidden');
+            burger.close();
         })
 
         window.addEventListener('scroll', () => {
@@ -418,6 +420,38 @@ class App {
 
                 })
             })
+        }
+    }
+
+    function burgerHandler() {
+        let burger = document.querySelector('[data-burger]');
+        if(burger) {
+            let span1 = burger.querySelector('span:nth-child(1)');
+            let span2 = burger.querySelector('span:nth-child(2)');
+            let span3 = burger.querySelector('span:nth-child(3)');
+            let span4 = burger.querySelector('span:nth-child(4)');
+    
+            return {
+                el: burger,
+                toggle() {
+                    span1.classList.toggle('first');
+                    span2.classList.toggle('second');
+                    span3.classList.toggle('third');
+                    span4.classList.toggle('fourth');
+                },
+                close() {
+                    span1.classList.remove('first');
+                    span2.classList.remove('second');
+                    span3.classList.remove('third');
+                    span4.classList.remove('fourth');
+                },
+                open() {
+                    span1.classList.add('first');
+                    span2.classList.add('second');
+                    span3.classList.add('third');
+                    span4.classList.add('fourth');
+                }
+            }
         }
     }
 }
